da34896c90a7c13b06a7d3c3a7e8d12aa1a4c663
arunima18 <42172417+arunima18@users.noreply.github.com>
2020-08-18 13:07:28 +0530

    Update Lab2_Qn1b.cpp

--- Lab2_Qn1b.cpp
+++ Lab2_Qn1b.cpp
@@ -1,7 +1,10 @@
+//Average distance between twopoints in a matrix
+
 #include<iostream>
 using namespace std;
 int main()
 {
+    //Compute distance between two points for a row
     int i=1;
     int j=1;
     float d=0;
@@ -17,9 +20,9 @@ int main()
                 }
             i=i+1;
         }
-    cout<<d<<endl;
     
-    //expand to matrices
+    
+    //expand to matrices using the value of d
     int p=1;
     int q=1;
     int sum1=0;
@@ -29,20 +32,20 @@ int main()
         q=1;
         while (q<=(7-p))
         {
+            //forward sum
             sum1=sum1+ ((q-1)*36) +d;
-            cout<<sum1<<"***"<<endl;
+            //backward sum
             sum2=sum2+((q-1)*6*6)+d;
-            //cout<<sum2<<"####"<<endl;
             q=q+1;
         }
         p=p+1;
     }
     
-    cout<<sum1<<endl;
-    cout<<sum2<<endl;
-    int y=sum1+sum2-6*d;
-    cout<<sum1+sum2<<endl;
-    cout<<y<<endl;
+    
+    //sum1+sum2 contains an extra distance of 6*d due to backward sum calculation, which has already being counted in the forward sum
+    float y=sum1+sum2-6*d;
+    float avg=y/(36*36);
+    cout<<"The average distance between any two points is: "<<avg<<endl;
             
     
     
